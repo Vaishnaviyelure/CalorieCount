57415df46c0d0d030a4ad2363c8fc2492d7d931d
vaishnavi <vaishnaviyelure24@gmail.com>
2022-09-08 11:52:51 +0530

    first commit

---
+++ app/src/main/java/in/starbow/fitindia/dashboard.kt
@@ -0,0 +1,36 @@
+package `in`.starbow.fitindia
+
+import `in`.starbow.fitindia.service.StepDetectorService
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        bmi.setOnClickListener {
+            Toast.makeText(applicationContext, "BMI", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, `in`.starbow.fitindia.bmi::class.java)
+            startActivity(intent);
+        }
+
+        pedometer.setOnClickListener {
+            Toast.makeText(applicationContext, "Pedometer", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, peodmeter::class.java)
+            startActivity(intent);
+        }
+      image_selector.setOnClickListener{
+          val intent = Intent(this,FoodSelectActivity::class.java)
+          startActivity(intent)
+      }
+
+    }
+}
